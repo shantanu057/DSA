19ecf2f042b29d30f17d2be7df0fac055854ae06
shantanu057 <shantanu057@gmail.com>
2022-08-17 23:27:56 +0530

    commit

--- Pattern.cpp
+++ Pattern.cpp
@@ -15,19 +15,158 @@ int main()
     // }
     // cout<<endl;
     
-    int z;
-    cin>>z;
-    int count=1;
-    for(int i=1;i<=z;i++)
-    {
-        for(int j=1;j<=z;j++)         //123
-                                      //456
-                                      //789
-        {
-            cout<<count;
-            count++;
-        }
-        cout<<endl;
-    }
+    // int z;
+    // cin>>z;
+    // int count=1;
+    // for(int i=1;i<=z;i++)
+    // {
+    //     for(int j=1;j<=z;j++)         //123
+    //                                   //456
+    //                                   //789
+    //     {
+    //         cout<<count;
+    //         count++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int p;
+    // cin>>p;
+
+    // for(int i=1;i<=p;i++)
+    // {
+    //     for(int j=1;j<=i;j++)               //*
+    //     {                                   //**    
+    //         cout<<"*";                      //***
+    //     }                                   //****
+    //     cout<<endl;
+    // }
+
+    // int q;
+    // cin>>q;
+
+    // for(int i=1;i<=q;i++)
+    // {
+    //     for(int j=1;j<=i;j++)               //1
+    //     {                                   //22
+    //         cout<<i;                        //333
+    //     }                                   //4444
+    //     cout<<endl;
+    // }
+
+    // int a;
+    // cin>>a;
+    // int count=1;
+    // for(int i=1;i<=a;i++)
+    // {
+    //     for(int j=1;j<=i;j++)                   //1
+    //     {                                       //23
+    //         cout<<count;                        //456
+    //         count++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int b;
+    // cin>>b;
+
+    // for(int i=1;i<=b;i++)
+    // {
+    //     for(int j=i;j>=1;j--)               //1
+    //     {                                   //21
+    //         cout<<j;                        //321
+    //     }                                   //4321
+    //     cout<<endl;
+    // }
+
+    // int c;
+    // cin>>c;
+    // char z=65;
+    // for(int i=1;i<=c;i++)                   //AAA
+    // {                                       //BBB
+    //     for(int j=1;j<=c;j++)               //CCC
+    //     {
+    //         cout<<z;
+    //     }
+    //     cout<<endl;
+    //     z++;
+    // }
+
+    // int a;
+    // cin>>a;
+    // char b=65;
+
+    // for(int i=1;i<=a;i++)               //ABC
+    // {                                   //ABC   
+    //     for(int j=1;j<=a;j++)           //ABC
+    //     {
+    //         cout<<b;
+    //         b++;
+    //     }
+    //     cout<<endl;
+    //     b=65;
+    // }
+
+    // int a;
+    // cin>>a;
+    // char z=65;
+
+    // for(int i=1;i<=a;i++)
+    // {
+    //     for(int j=1;j<=a;j++)       //ABC
+    //     {                           //DEF
+    //         cout<<z;                //GHI
+    //         z++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int a;
+    // cin>>a;
+    // char b=65;
+
+    // for(int i=1;i<=a;i++)
+    // {
+    //     for(int j=1;j<=a;j++)       //ABC
+    //     {                           //BCD
+    //         cout<<b;                //CDE
+    //         b++;
+    //     }
+    //     cout<<endl;
+    //     b-=2;
+    // }
+
+    // int a;
+    // cin>>a;
+    // char b=65;
+
+    // for(int i=1;i<=a;i++)
+    // {
+    //     for(int j=1;j<=i;j++)           //A
+    //     {                               //BB        
+    //         cout<<b;                    //CCC
+    //     }
+    //     b++;
+    //     cout<<endl;
+    // }
+
+//    int a;
+//    cin>>a;
+
+//    for(int i=1;i<=a;i++)
+//    {
+//         for(int j=1;j<=a-i;j++)         //   *
+//         {                               //  **
+//             cout<<" ";                  // ***
+//         }
+//         for(int k=1;k<=i;k++)
+//         {
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//    }
+
+    //52
+
     return 0;
 }
